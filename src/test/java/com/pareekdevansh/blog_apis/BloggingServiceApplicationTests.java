6000c1d030b750f7a029479decc1ae85c2d27e40
Devansh Pareek <devanshpareek863@gmail.com>
2024-06-02 17:34:54 +0530

    feat: initial project setup with mysql db

---
+++ src/test/java/com/pareekdevansh/blog_apis/BloggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pareekdevansh.blog_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
